5446bf46c33eb7d60ed8fbb8ecaa273f1cf490dc
wenxuan.ding <wenxuan.ding@pintec.com>
2021-04-29 10:29:28 +0800

    add

---
+++ src/test/java/wiki/dwx/allinone/AllinoneApplicationTests.java
@@ -0,0 +1,13 @@
+package wiki.dwx.allinone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllinoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
