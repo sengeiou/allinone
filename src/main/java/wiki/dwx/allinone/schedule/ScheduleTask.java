5f3ca4f2900282087cbf62873595e1eefde1b91f
丁文选 <wenxuan.ding@pintec.com>
2021-06-06 09:38:06 +0800

    Scheduled

--- src/main/java/wiki/dwx/allinone/schedule/ScheduleTask.java
+++ src/main/java/wiki/dwx/allinone/schedule/ScheduleTask.java
@@ -19,7 +19,7 @@ public class ScheduleTask {
     @Resource
     private WeatherSMSService weatherSMSService;
 
-//    @Scheduled(cron = "0 0 0/1 * * ? ")
+    @Scheduled(cron = "0 0 7,9,11,13,15,17,19,21 * * ?")
     public void doTasks() throws WxErrorException {
         final WxCpService wxCpService = WxCpConfig.getCpService(1000002);
 
