62fbca03d52123f2dcd0ffc00caacb3299630bf0
丁文选 <wenxuan.ding@pintec.com>
2021-06-06 11:31:03 +0800

    result

--- src/main/java/wiki/dwx/allinone/schedule/ScheduleTask.java
+++ src/main/java/wiki/dwx/allinone/schedule/ScheduleTask.java
@@ -5,10 +5,12 @@ import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.cp.api.WxCpService;
 import me.chanjar.weixin.cp.bean.article.NewArticle;
 import me.chanjar.weixin.cp.bean.message.WxCpMessage;
+import me.chanjar.weixin.cp.bean.message.WxCpMessageSendResult;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import wiki.dwx.allinone.config.WxCpConfig;
 import wiki.dwx.allinone.service.WeatherSMSService;
+import wiki.dwx.allinone.utils.DateUtils;
 
 import javax.annotation.Resource;
 import java.util.Map;
@@ -36,7 +38,8 @@ public class ScheduleTask {
                     .toUser("DingWenXuan")
                     .addArticle(article)
                     .build();
-            wxCpService.getMessageService().send(message);
+            WxCpMessageSendResult result = wxCpService.getMessageService().send(message);
+            log.info(result.toString());
         } catch (Exception e) {
             Map res = weatherSMSService.getWeather4Wc("101010300");
             NewArticle article = NewArticle.builder()
@@ -50,7 +53,55 @@ public class ScheduleTask {
                     .toUser("DingWenXuan")
                     .addArticle(article)
                     .build();
-            wxCpService.getMessageService().send(message);
+            WxCpMessageSendResult result = wxCpService.getMessageService().send(message);
+            log.info(result.toString());
         }
+    }
+
+    @Scheduled(cron = "0 1 7,12,18,22 * * ?")
+    public void configureTasks() throws WxErrorException {
+        log.info("定时任务时间: " + DateUtils.toTimeString(DateUtils.getNowDate()));
+        final WxCpService wxCpService = WxCpConfig.getCpService(1000002);
+        try {
+            Map res = weatherSMSService.getWeather4Wc("101220110");
+            NewArticle article = NewArticle.builder()
+                    .title(res.get("title").toString())
+                    .description(res.get("msg").toString())
+                    .picUrl(res.get("img").toString())
+                    .url(res.get("fxLink").toString())
+                    .build();
+            WxCpMessage message = WxCpMessage.NEWS()
+                    .agentId(1000002)
+                    .toUser("chloed|davidlu")
+                    .addArticle(article)
+                    .build();
+            WxCpMessageSendResult result = wxCpService.getMessageService().send(message);
+            log.info(result.toString());
+        } catch (Exception e) {
+            Map res = weatherSMSService.getWeather4Wc("101220110");
+            NewArticle article = NewArticle.builder()
+                    .title(res.get("title").toString())
+                    .description(res.get("msg").toString())
+                    .picUrl(res.get("img").toString())
+                    .url(res.get("fxLink").toString())
+                    .build();
+            WxCpMessage message = WxCpMessage.NEWS()
+                    .agentId(1000002)
+                    .toUser("chloed|davidlu")
+                    .addArticle(article)
+                    .build();
+            WxCpMessageSendResult result = wxCpService.getMessageService().send(message);
+            log.info(result.toString());
+        }
+
+//        try {
+//            String sms2 = weatherSMSService.getWeatherSMS("101010700");
+//            log.info(sms2);
+//            sctFtqqService.sendMSG("北京昌平天气预报", sms2, "SCT4256T1qVLRM9qwwAQ45wUqeY1HE91");
+//        } catch (Exception e) {
+//            String sms2 = weatherSMSService.getWeatherSMS("101010700");
+//            log.info(sms2);
+//            sctFtqqService.sendMSG("北京昌平天气预报", sms2, "SCT4256T1qVLRM9qwwAQ45wUqeY1HE91");
+//        }
     }
 }
--- src/main/java/wiki/dwx/allinone/utils/DateUtils.java
+++ src/main/java/wiki/dwx/allinone/utils/DateUtils.java
@@ -1,14 +1,31 @@
 package wiki.dwx.allinone.utils;
 
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+
 import java.awt.*;
 import java.util.Date;
 
 public class DateUtils {
 
+    public final static String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
     private final static String[] week = {"", "一", "二", "三", "四", "五", "六", "日"};
 
     public static Date getNowDate() {
         return new Date();
+    }
+
+    public static String toTimeString(Date date) {
+        return toTimeString(date, DATE_TIME_FORMAT);
+    }
+
+    public static String toTimeString(Date date, String formatStr) {
+        if (date == null) {
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(formatStr);
     }
 
     public static String getDayOfWeek(int d) {
